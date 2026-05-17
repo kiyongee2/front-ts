a8bc4a8e0e3ea239d0303c38a3d336468ee53ca6
kiyongee2 <kiyongee2@gmail.com>
2026-05-17 12:29:28 +0900

    next-app

--- next_app/app/other/[name]/page.tsx
+++ next_app/app/other/[name]/page.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+import styles from "../style.module.css";
 
 export default async function Name({ params }: { params: Promise<{ name: string }> }) {
   const { name } = await params;
   return (
     <main>
-      <h1 className="title">Name Page</h1>
-      <p className="msg">Hello, {name}!</p>
+      <h1 className={styles.title}>Name Page</h1>
+      <p className={styles.msg}>당신은 {name}님 이군요!</p>
       <div>
-        <a href="/" className="link">Go to Home</a>
+        <Link href="/" className={styles.link}>Go to Home</Link>
       </div>
     </main>
   );
--- next_app/app/other/page.tsx
+++ next_app/app/other/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 // Image 컴포넌트 사용을 위해 import 추가
 import Image from "next/image";
+import styles from "./style.module.css";
 
 export default function Other() {
   return (
     <main>
-      <h1 className="title">Other Page</h1>
+      <h1 className={styles.title}>Other Page</h1>
+      <p className={styles.msg}>This is the Other page.</p>
       <div>
         <Image src="/tatu.jpg" width={300} height={300} alt="Tatu" /> 
       </div>
